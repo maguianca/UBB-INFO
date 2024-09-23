63cf27d0b692aa42aa98983c07b324162e9758f2
maguianca <150610171+maguianca@users.noreply.github.com>
2024-09-23 23:08:02 +0300

    Add files via upload

---
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/L4_cofetarie/L4_cofetarie/MateriePrima.c
@@ -0,0 +1,56 @@
+#include <string.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include "MateriePrima.h"
+
+/*
+ * Creeaza o entitate de tipul Materie Prima
+ * param nume: numele entitatii (char)
+ * param producator: producatorul entitatii (char)
+ * param cantitate: cantitatea (int)
+ * return: Materie Prima
+ */
+MateriePrima creeazaMateriePrima(char* nume, char* producator, int cantitate) {
+    MateriePrima m;
+    int nrC = (int)strlen(nume) + 1;
+    m.nume = malloc(nrC * sizeof(char));
+    strcpy_s(m.nume,20, nume);
+
+    nrC = (int)strlen(producator) + 1;
+    m.producator = malloc(nrC * sizeof(char));
+    strcpy_s(m.producator,20,producator);
+
+    m.cantitate = cantitate;
+    return m;
+}
+
+/*
+ * Valideaza o entitate de tipul Materie Prima
+ * param: m (struct)
+ * return: 1 - daca entitatea este valida
+ *         0 - daca entitatea nu este valida
+ */
+int valideazaMateriePrima(MateriePrima m) {
+    if (strlen(m.nume) == 0)
+        return 0;
+    if (strlen(m.producator) == 0)
+        return 0;
+    if (m.cantitate < 0)
+        return 0;
+    return 1;
+}
+
+/*
+ * Sterge elementele entitatii de tipul Materie Prima
+ * param: m (struct)
+ */
+void destroyMateriePrima(MateriePrima* m) {
+    //    m -> nume[0] = '\0';
+    //    m -> producator[0] = '\0';
+    free(m->nume);
+    free(m->producator);
+    m->cantitate = -1;
+    m->nume = NULL;
+    m->producator = NULL;
+
+}
---
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/L4_cofetarie/L4_cofetarie/teste.h
@@ -0,0 +1,28 @@
+#pragma once
+void test_creeazaMateriePrima();
+
+void test_destroyMateriePrima();
+
+void test_valideazaMateriePrima();
+
+void test_createEmpty();
+
+void test_add();
+
+void test_addCantitate();
+
+void test_delete();
+
+void test_get();
+
+void test_size();
+
+void test_adaugaMateriePrima();
+
+void test_stergeMateriePrima();
+
+void test_modificaMateriePrima();
+
+void test_filtruMateriePrima();
+
+void test_sortMateriePrima();
