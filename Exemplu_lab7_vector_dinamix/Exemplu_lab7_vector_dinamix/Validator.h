ef15287e2789e5120c1c7b0ef4f6429ea375b3be
maguianca <150610171+maguianca@users.noreply.github.com>
2024-09-23 23:11:21 +0300

    Delete Exemplu_lab7_vector_dinamix directory

--- Exemplu_lab7_vector_dinamix/Exemplu_lab7_vector_dinamix/Validator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#pragma once
-#include <string>
-using namespace std;
-
-class Validator {
-public:
-	void validateOferta(string denumire, string destinatie, string tip, int pret);
-};
--- Exemplu_lab7_vector_dinamix/Exemplu_lab7_vector_dinamix/teste.h
+++
@@ -1,7 +0,0 @@
-#include "OfertaRepo.h"
-#include "AgentieService.h"
-#include "VectorDinamic.h"
-#include "Validator.h"
-#include "Errors.h"
-
-void test_all();
