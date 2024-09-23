46ec71a5c7773597ecc41f06f539e126abd6a24a
maguianca <150610171+maguianca@users.noreply.github.com>
2024-09-23 23:21:54 +0300

    Add files via upload

---
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/Laborator 8/Laborator 8/repo.h
@@ -0,0 +1,49 @@
+#pragma once
+#pragma once
+#include<vector>
+#include<unordered_map>
+#include"domeniu.h"
+//#include"vector.h"
+using std::vector;
+class RepoException {
+	string msg;
+public:
+	RepoException(string m) :msg{ m } {
+
+	}
+	string get_mesaj() {
+		return msg;
+	}
+
+};
+class Repo
+{
+private:
+	vector<Turism>vacante;
+public:
+	//Constructor Repo
+	Repo(const Repo& ot) = delete;
+	Repo() = default;
+	void store(const Turism& oferta);
+	/*
+	* stocheaza un obiect Turism in lista de vacante
+	* return:none
+	*/
+	void stergere(const Turism& oferta);
+	/*
+		sterge un obiect de tip turism din lista de vacante
+	*/
+	const Turism& cauta(const Turism& o);
+	/*
+	* cauta un obuiect de tip turism in lista de oferte si returneaza elementul
+	*/
+	void modifica(const Turism& o);
+	/*
+	modifica un element de tip turim cu parametrii tip si pret din obiectul transmis prin parametru*/
+	const vector<Turism>& get_all()const
+	{
+		//returneaza lista totala de obiecte
+		return vacante;
+	}
+	bool exist(const Turism& o);
+};
