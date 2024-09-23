9f3930d81f7dfb504430ec5d9bae65eb83fceacf
maguianca <150610171+maguianca@users.noreply.github.com>
2024-09-23 20:18:15 +0300

    Add files via upload

---
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/PREGATIRE EXAMAN/Tudor_Xsi0/Tudor_Xsi0/service.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma once
+#include "Repo.h"
+
+class Service {
+	Repo& repo;
+
+public:
+	Service(Repo& r) : repo(r) {}
+	string pressed(int i, int j);
+};
+
+
+string Service::pressed(int i, int j)
+{
+	return repo.pressed(i, j);
+}
